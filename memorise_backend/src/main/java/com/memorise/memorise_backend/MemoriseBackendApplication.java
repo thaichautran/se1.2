2b40928d44adfd6afcff7c828ac068477d7d5016
thaichautran <133397705+thaichautran@users.noreply.github.com>
2023-11-01 21:22:02 +0700

    Add files via upload

---
+++ memorise_backend/src/main/java/com/memorise/memorise_backend/MemoriseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.memorise.memorise_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoriseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoriseBackendApplication.class, args);
+	}
+
+}
