49fd0c5f5f2ccbcd846ee0a5ddbd9562cba4956c
tranthaichau <chautt2003@gmail.com>
2023-11-01 21:32:41 +0700

    Hello world with backend

--- memorise_backend/src/main/java/com/memorise/memorise_backend/MemoriseBackendApplication.java
+++ memorise_backend/src/main/java/com/memorise/memorise_backend/MemoriseBackendApplication.java
@@ -8,6 +8,7 @@ public class MemoriseBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MemoriseBackendApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }
